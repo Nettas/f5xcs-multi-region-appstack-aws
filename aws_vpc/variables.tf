e228eabb358dc21615d349b5ab5dca93c48afec5
Nettas <nettas@aol.com>
2023-03-03 14:41:59 -0500

    added aws vpc build-out

---
+++ aws_vpc/variables.tf
@@ -0,0 +1,26 @@
+variable "environment" {
+	default		= "sse-xc-aws"
+	description = "Environment Name"
+}
+
+variable "aws_access_key" {
+  description = "REQUIRED: AWS programatic access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "REQUIRED: AWS programatic secret key"
+  type        = string
+}
+
+variable "aws_region" {
+	description = "Region in which environment will be launched"
+	type = string
+	default = "us-east-2"
+}
+
+variable "aws_vpc_cidr" {
+	type 		= string
+	default 	= "10.20.0.0/16"
+	description = "CIDR block of the vpc"
+}
---
+++ aws_vpc/vpc.tf
@@ -0,0 +1,100 @@
+# Initialize availability zone data from AWS
+data "aws_availability_zones" "available" {}
+
+# Vpc resource
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.aws_vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+### Create IGW ###
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name        = "${var.environment}-igw"
+    Environment = var.environment
+  }
+}
+
+### Subnet public ###
+resource "aws_subnet" "public_subnet" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.20.${10+count.index}.0/24"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-pubsubnet"
+    Environment = var.environment
+  }
+}
+
+### Subnet ###
+resource "aws_subnet" "private_subnet" {
+  count                   = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.20.${20+count.index}.0/24"
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name        = "${var.environment}-privsubnet"
+    Environment = var.environment
+  }
+}
+
+# Routing table for public subnets
+resource "aws_route_table" "rtblPublic" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig.id}"
+  }
+
+  tags = {
+    Name        = "${var.environment}-rt_tbl"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "route" {
+  count          = "${length(data.aws_availability_zones.available.names)}"
+  subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.rtblPublic.id}"
+}
+
+# # Elastic IP for NAT gateway
+# resource "aws_eip" "nat" {
+#   vpc = true
+# }
+
+# # NAT Gateway
+# resource "aws_nat_gateway" "nat-gw" {
+#   allocation_id = "${aws_eip.nat.id}"
+#   subnet_id     = "${element(aws_subnet.private_subnet.*.id, 1)}"
+#   depends_on    = ["aws_internet_gateway.myInternetGateway"]
+# }
+
+# Routing table for private subnets
+# resource "aws_route_table" "rtblPrivate" {
+#   vpc_id = aws_vpc.vpc.id
+
+#   tags = {
+#     Name        = "${var.environment}-privrtb"
+#     Environment = var.environment
+#   }
+# }
+
+# resource "aws_route_table_association" "private_route" {
+#   count          = "${length(data.aws_availability_zones.available.names)}"
+#   subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+#   route_table_id = "${aws_route_table.rtblPrivate.id}"
+# }
