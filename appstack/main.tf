84a1c108701608097ae7857db14406db4aa23bfc
Nettas <nettas@aol.com>
2023-03-04 07:58:53 -0500

    adding site

---
+++ appstack/main.tf
@@ -0,0 +1,365 @@
+# Create Random ID to Append to Builds to Avoid Naming Conflicts
+resource "random_id" "buildSuffix" {
+  byte_length = 2
+}
+
+resource "tls_private_key" "newkey" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_sensitive_file" "newkey_pem" {
+  # create a new local ssh identity
+  filename        = "${abspath(path.root)}/.ssh/${var.project_prefix}-key-${random_id.buildSuffix.hex}.pem"
+  content         = tls_private_key.newkey.private_key_pem
+  file_permission = "0400"
+}
+
+resource "aws_key_pair" "deployer" {
+  # create a new AWS ssh identity
+  key_name   = "${var.project_prefix}-key-${random_id.buildSuffix.hex}"
+  public_key = tls_private_key.newkey.public_key_openssh
+  tags = {
+    Owner = var.resource_owner
+  }
+}
+
+# Create IAM policy
+resource "aws_iam_policy" "f5xc_iam_policy" {
+  name = "f5xc_iam_policy_${random_id.buildSuffix.hex}"
+  path = "/"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            "Action" = [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeInstances",
+                "ec2:DescribeRegions",
+                "ec2:DescribeRouteTables",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVolumes",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateTags",
+                "ec2:CreateVolume",
+                "ec2:ModifyInstanceAttribute",
+                "ec2:ModifyVolume",
+                "ec2:AttachVolume",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:CreateRoute",
+                "ec2:DeleteRoute",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteVolume",
+                "ec2:DetachVolume",
+                "ec2:RevokeSecurityGroupIngress",
+                "ec2:DescribeVpcs",
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:AttachLoadBalancerToSubnets",
+                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:CreateLoadBalancerPolicy",
+                "elasticloadbalancing:CreateLoadBalancerListeners",
+                "elasticloadbalancing:ConfigureHealthCheck",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:DeleteLoadBalancerListeners",
+                "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:DescribeLoadBalancerAttributes",
+                "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:ModifyLoadBalancerAttributes",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+                "elasticloadbalancing:AddTags",
+                "elasticloadbalancing:CreateListener",
+                "elasticloadbalancing:CreateTargetGroup",
+                "elasticloadbalancing:DeleteListener",
+                "elasticloadbalancing:DeleteTargetGroup",
+                "elasticloadbalancing:DescribeListeners",
+                "elasticloadbalancing:DescribeLoadBalancerPolicies",
+                "elasticloadbalancing:DescribeTargetGroups",
+                "elasticloadbalancing:DescribeTargetHealth",
+                "elasticloadbalancing:ModifyListener",
+                "elasticloadbalancing:ModifyTargetGroup",
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+                "iam:CreateServiceLinkedRole",
+                "kms:DescribeKey"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+
+  })
+}
+
+# Create a role
+resource "aws_iam_role" "f5xc_role" {
+  name = "f5xc_role_${random_id.buildSuffix.hex}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    "Statement" = [
+        {
+            "Sid" = "",
+            "Effect" = "Allow",
+            "Principal" = {
+                "Service" = "ec2.amazonaws.com"
+            },
+            "Action" = "sts:AssumeRole"
+        }
+    ]
+  })
+}
+
+# Attach role to policy
+resource "aws_iam_policy_attachment" "f5xc_policy_role" {
+  name = "f5xc_policy_role_${random_id.buildSuffix.hex}"
+  roles = [aws_iam_role.f5xc_role.name]
+  policy_arn = aws_iam_policy.f5xc_iam_policy.arn
+}
+
+# Attach role to an instance profile
+resource "aws_iam_instance_profile" "f5xc_profile" {
+  name = "f5xc_profile_${random_id.buildSuffix.hex}"
+  role = aws_iam_role.f5xc_role.name
+}
+# Create Network Interfaces for Customer Edges
+resource "aws_network_interface" "f5xc_ce1_inside" {
+  subnet_id                 = var.ce1_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce1_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce1_outside" {
+  subnet_id                 = var.ce1_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce1_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_eip" "f5xc_ce1_outside" {
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce1_outside.id
+  associate_with_private_ip = aws_network_interface.f5xc_ce1_outside.private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce1_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce2_inside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  subnet_id                 = var.ce2_inside_subnet_id != "" ? var.ce2_inside_subnet_id : var.ce1_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce2_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce2_outside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  subnet_id                 = var.ce2_outside_subnet_id != "" ? var.ce2_outside_subnet_id : var.ce1_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce2_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_eip" "f5xc_ce2_outside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce2_outside[0].id
+  associate_with_private_ip = aws_network_interface.f5xc_ce2_outside[0].private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce2_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce3_inside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  subnet_id                 = var.ce3_inside_subnet_id != "" ? var.ce3_inside_subnet_id : var.ce1_inside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.inside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce3_inside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_network_interface" "f5xc_ce3_outside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  subnet_id                 = var.ce3_outside_subnet_id != "" ? var.ce3_outside_subnet_id : var.ce1_outside_subnet_id
+  private_ips_count         = 1
+  security_groups           = [var.outside_security_group]
+  source_dest_check         = false
+  private_ip_list_enabled   = false
+  ipv6_address_list_enabled = false
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_ce3_outside-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_eip" "f5xc_ce3_outside" {
+  count                     = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  vpc                       = true
+  network_interface         = aws_network_interface.f5xc_ce3_outside[0].id
+  associate_with_private_ip = aws_network_interface.f5xc_ce3_outside[0].private_ip
+  tags = {
+    Name  = "${var.project_prefix}-f5xc_ce_az3_outside_eipd-${random_id.buildSuffix.hex}"
+    Owner = var.resource_owner
+  }
+}
+
+resource "aws_instance" "f5xc_ce1" {
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.f5xc_profile.name
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az1
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}",
+      sitesshrsakey = "${tls_private_key.newkey.private_key_pem}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce1_outside.id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce1_inside.id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-0"
+    "kubernetes.io/cluster/${var.clustername}" = "owned"
+    ves-io-site-name = "${var.clustername}"
+  }
+}
+
+resource "aws_instance" "f5xc_ce2" {
+  count         = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.f5xc_profile.name
+
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az2
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}",
+      sitesshrsakey = "${tls_private_key.newkey.private_key_pem}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce2_outside[0].id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce2_inside[0].id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-1"
+    "kubernetes.io/cluster/${var.clustername}" = "owned"
+    ves-io-site-name = "${var.clustername}"
+  }
+}
+
+resource "aws_instance" "f5xc_ce3" {
+  count         = var.f5xc_ce_gateway_multi_node ? 1 : 0
+  ami           = var.amis[var.aws_region]
+  instance_type = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.f5xc_profile.name
+
+  root_block_device {
+    volume_size = var.instance_disk_size
+    volume_type = "gp3"
+  }
+  get_password_data = false
+  monitoring        = false
+  availability_zone = var.az3
+
+  user_data_replace_on_change = true
+  user_data = templatefile("${path.module}/cloud_init.yaml.template",
+    {
+      sitetoken     = "${var.sitetoken}",
+      clustername   = "${var.clustername}",
+      sitelatitude  = "${var.sitelatitude}",
+      sitelongitude = "${var.sitelongitude}",
+      sitesshrsakey = "${tls_private_key.newkey.private_key_pem}"
+    }
+  )
+
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce3_outside[0].id
+    device_index         = 0
+  }
+  network_interface {
+    network_interface_id = aws_network_interface.f5xc_ce3_inside[0].id
+    device_index         = 1
+  }
+  tags = {
+    Name = "${var.project_prefix}-master-2"
+    "kubernetes.io/cluster/${var.clustername}" = "owned"
+    ves-io-site-name = "${var.clustername}"
+  }
+}
---
+++ appstack/variables.tf
@@ -0,0 +1,152 @@
+variable "aws_region" {
+  description = "REQUIRED: AWS Region to deploy the Customer Edge into"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "REQUIRED: AWS programatic access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "REQUIRED: AWS programatic secret key"
+  type        = string
+}
+
+variable "f5xc_ce_gateway_multi_node" {
+  description = "OPTIONAL: Set to true to deploy a 3 node cluster of Customer Edges"
+  type        = bool
+  default     = true
+}
+variable "az1" {
+  description = "OPTIONAL: AWS availability zone to deploy first Customer Edge into"
+  type        = string
+  default     = ""
+}
+
+variable "az2" {
+  description = "OPTIONAL: AWS availability zone to deploy second Customer Edge into"
+  type        = string
+  default     = ""
+}
+
+variable "az3" {
+  description = "OPTIONAL: AWS availability zone to deploy third Customer Edge into"
+  type        = string
+  default     = ""
+}
+
+variable "project_prefix" {
+  description = "OPTIONAL: Provide a project name prefix that will be applied"
+  type        = string
+  default     = "demo"
+}
+
+variable "resource_owner" {
+  description = "OPTIONAL: Provide owner of the deployment for tagging purposes"
+  type        = string
+  default     = "demo.user"
+}
+
+variable "ce1_outside_subnet_id" {
+  description = "REQUIRED: The AWS subnet ID for the outside subnet of Customer Edge 1"
+  type        = string
+}
+
+variable "ce1_inside_subnet_id" {
+  description = "REQUIRED: The AWS subnet ID for the inside subnet of Customer Edge 1"
+  type        = string
+}
+
+
+variable "ce2_outside_subnet_id" {
+  description = "OPTIONAL: The AWS subnet ID for the outside subnet of Customer Edge 2"
+  type        = string
+  default     = ""
+}
+
+variable "ce2_inside_subnet_id" {
+  description = "OPTIONAL: The AWS subnet ID for the inside subnet of Customer Edge 2"
+  type        = string
+  default     = ""
+}
+
+variable "ce3_outside_subnet_id" {
+  description = "OPTIONAL: The AWS subnet ID for the outside subnet of Customer Edge 3"
+  type        = string
+  default     = ""
+}
+
+variable "ce3_inside_subnet_id" {
+  description = "OPTIONAL: The AWS subnet ID for the inside subnet of Customer Edge 3"
+  type        = string
+  default     = ""
+
+}
+
+variable "outside_security_group" {
+  description = "REQUIRED: The AWS security group ID for the outside interfaces"
+  type        = string
+}
+
+variable "inside_security_group" {
+  description = "REQUIRED: The AWS security group ID for the inside interfaces"
+  type        = string
+}
+
+
+variable "amis" {
+  description = "REQUIRED: The AWS amis for the Customer Edge image"
+  type        = map(any)
+  default = {
+    "ca-central-1"   = "ami-052252c245ff77338"
+    "af-south-1"     = "ami-0c22728f79f714ed1"
+    "ap-east-1"      = "ami-0a6cf3665c0612f91"
+    "ap-northeast-2" = "ami-01472d819351faf92"
+    "ap-southeast-2" = "ami-03ff18dfb7f90eb54"
+    "ap-south-1"     = "ami-0277ab0b4db359c93"
+    "ap-northeast-1" = "ami-0384d075a36447e2a"
+    "ap-southeast-1" = "ami-0d6463ee1e3727e84"
+    "eu-central-1"   = "ami-06d5e0073d97ecf99"
+    "eu-west-1"      = "ami-090680f491ad6d46a"
+    "eu-west-3"      = "ami-03bd7c41ca1b586a8"
+    "eu-south-1"     = "ami-0baafa10ffcd081b7"
+    "eu-north-1"     = "ami-006c465449ed98c69"
+    "eu-west-2"      = "ami-0df8a483722043a41"
+    "me-south-1"     = "ami-094efc1a78169dd7c"
+    "sa-east-1"      = "ami-07369c4b06cf22299"
+    "us-east-1"      = "ami-089311edbe1137720"
+    "us-east-2"      = "ami-01ba94b5a83adcb35"
+    "us-west-1"      = "ami-092a2a07d2d3a445f"
+    "us-west-2"      = "ami-07252e5ab4023b8cf"
+  }
+}
+
+variable "instance_type" {
+  description = "REQUIRED: The AWS instance type for the Customer Edge"
+  type        = string
+  default     = "t3.xlarge"
+}
+
+variable "instance_disk_size" {
+  description = "OPTIONAL: The AWS disk size for the Customer Edge"
+  type        = string
+  default     = "40"
+}
+variable "sitelatitude" {
+  description = "REQUIRED: Site Physical Location Latitude. See https://www.latlong.net/"
+  type        = string
+}
+variable "sitelongitude" {
+  description = "REQUIRED: Site Physical Location Longitude. See https://www.latlong.net/"
+  type        = string
+}
+variable "clustername" {
+  description = "REQUIRED: Customer Edge site cluster name."
+  type        = string
+}
+variable "sitetoken" {
+  description = "REQUIRED: Distributed Cloud Customer Edge site registration token."
+  type        = string
+  sensitive   = true
+}
